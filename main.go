fa3e36a5c75687c96d27ed2ef7620d58aabbebf8
John Wregglesworth <wregglej@iplantcollaborative.org>
2016-09-23 09:46:26 -0700

    Add main.go

---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+)
+
+var (
+	dockerURI = flag.String("docker-uri", "unix:///var/run/docker.sock", "The docker URI.")
+	input     = flag.String("input", "", "The file to write the JSON to.")
+)
+
+// ImageInfo contains the information about each image that needs to be included
+// in the manifest.
+type ImageInfo struct {
+	RepoTag string `json:"repo-tag"`
+	ImageID string `json:"image-id"`
+	GitRef  string `json:"git-ref"`
+}
+
+// InputMap is the top-level struct that the JSON input is parsed into
+type InputMap struct {
+	Hostname string      `json:"hostname"`
+	Date     string      `json:"date"`
+	Images   []ImageInfo `json:"images"`
+}
+
+// NewInputMap reads the contents of the file at 'path' and parses it into a new
+// *InputMap.
+func NewInputMap(path string) (*InputMap, error) {
+	input, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	inputMap := &InputMap{}
+	err = json.Unmarshal(input, inputMap)
+	return inputMap, err
+}
+
+func main() {
+	flag.Parse()
+
+	if *input == "" {
+		log.Fatal("--input must be set")
+	}
+
+	defaultHeaders := map[string]string{"User-Agent": "engine-api-cli-1.0"}
+	d, err := client.NewClient(*dockerURI, "v1.22", nil, defaultHeaders)
+	if err != nil {
+		log.Fatalf("Error creating docker client: %s", err)
+	}
+
+	inputMap, err := NewInputMap(*input)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx := context.Background()
+
+	// grab a list of all of the local images.
+	listedImages, err := d.ImageList(ctx, types.ImageListOptions{All: true})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	valid := true
+	for _, li := range listedImages {
+		for _, listedRepoTag := range li.RepoTags {
+			for _, inputImage := range inputMap.Images {
+				if listedRepoTag == inputImage.RepoTag {
+					if li.ID != inputImage.ImageID {
+						fmt.Printf("Docker image ID does not match for %s: %s, %s\n", inputImage.RepoTag, li.ID, inputImage.ImageID)
+						valid = false
+					}
+				}
+			}
+		}
+	}
+	if !valid {
+		os.Exit(-1)
+	}
+}
